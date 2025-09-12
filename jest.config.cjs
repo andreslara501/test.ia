64d49a516b8992e69b3d59cd70117af56b83c71c
Andrés Lara <andreslara501@gmail.com>
2025-09-12 13:21:16 -0500

    First commit

---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+};
